c836ce0238569598b753dfdf96d986b5966cf7ec
Yuya Unno <unnonouno@gmail.com>
2012-04-13 19:40:20 +0900

    Check exception

--- maven/src/test/java/us/jubat/classifier/ClassifierTest.java
+++ maven/src/test/java/us/jubat/classifier/ClassifierTest.java
@@ -1,31 +1,28 @@
 package us.jubat.classifier;
 
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class ClassifierTest {
-    /*
-    public static void main(String[] args){
-	try{
+	@Before
+	public void setUp() throws Exception {
+		// super.setUp();
 	}
-    }
-    protected void setUp() throws Exception {
-	//super.setUp();
-    }
 
-    protected void tearDown() throws Exception {
-	//super.tearDown();
-    }
-    */
-    @Test
-    public void testHello() throws Exception {
-	ClassifierClient c = new ClassifierClient("localhost", 9199, 10);
-	
-	
-	//	c.set_config("");
-	c.get_config("");
-	///	System.out.println("hello");
-    }
-    @Test
-    public void testHello2(){
-    }
+	public void tearDown() throws Exception {
+		// super.tearDown();
+	}
+
+	@Test(expected = Exception.class)
+	public void testHello() throws Exception {
+		ClassifierClient c = new ClassifierClient("localhost", 9199, 10);
+
+		// c.set_config("");
+		c.get_config("");
+		// / System.out.println("hello");
+	}
+
+	@Test
+	public void testHello2() {
+	}
 }
